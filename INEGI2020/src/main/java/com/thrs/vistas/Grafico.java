45bc93b6bacb3186ebc9ac7307efef9f0df3e8d2
PeterThrs <peterlopezthrs@gmail.com>
2023-12-02 01:17:23 -0600

    mejoras al grafico

--- INEGI2020/src/main/java/com/thrs/vistas/Grafico.java
+++ INEGI2020/src/main/java/com/thrs/vistas/Grafico.java
@@ -8,6 +8,9 @@ import com.thrs.models.consultas.Estado;
 import com.thrs.services.graphicServices.GraficosAvanzadosService;
 import com.thrs.services.graphicServices.ObjGraficosService;
 import com.thrs.services.graphicServices.RecursosService;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.GradientPaint;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
@@ -15,29 +18,34 @@ import javax.swing.JPanel;
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
+import org.jfree.chart.axis.CategoryAxis;
+import org.jfree.chart.axis.CategoryLabelPositions;
+import org.jfree.chart.axis.NumberAxis;
+import org.jfree.chart.plot.CategoryPlot;
 import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.chart.renderer.category.BarRenderer;
 import org.jfree.data.category.DefaultCategoryDataset;
 
 /**
  *
  * @author Usuario
  */
-public class Grafico extends JPanel{
-    
+public class Grafico extends JPanel {
+
     private GridBagConstraints gbc;
     private ObjGraficosService sObjGraficos;
     private GraficosAvanzadosService sGraficosAvanzados;
     private RecursosService sRecursos;
     private Estado estado;
-    
-    public Grafico(Estado estado){
+
+    public Grafico(Estado estado) {
         this.estado = estado;
         this.setLayout(new GridBagLayout());
         this.sObjGraficos = ObjGraficosService.getService();
-        
+
         cargarGrafica();
     }
-    
+
     public void cargarGrafica() {
         DefaultCategoryDataset datos = new DefaultCategoryDataset();
 
@@ -62,21 +70,36 @@ public class Grafico extends JPanel{
                 null,
                 "Población",
                 datos,
-                PlotOrientation.HORIZONTAL.VERTICAL,
+                PlotOrientation.VERTICAL,
                 true,
                 true,
-                false
+                true
         );
-        
+
+        grafico_barras.setBackgroundPaint(Color.WHITE);//Dar color al fondo del panel
+        grafico_barras.getTitle().setPaint(Color.BLACK);//Dar color al titulo 
+
+        CategoryPlot plot = grafico_barras.getCategoryPlot();
+        plot.setBackgroundPaint(Color.WHITE);//Color del fondo del gr�fico
+
+        plot.setDomainGridlinesVisible(true);//Lineas divisorias
+        plot.setRangeGridlinePaint(Color.BLACK);//Color de lineas divisorias
+
         ChartPanel panel = new ChartPanel(grafico_barras);
         panel.setMouseWheelEnabled(true);
         
-        gbc = sObjGraficos.getGridBagConstraints(0, 0, 1, 1, 1, 1, GridBagConstraints.BOTH, GridBagConstraints.CENTER, new Insets(0, 0, 0, 0), 0, 0);
+        CategoryAxis domainAxis = plot.getDomainAxis();
+        domainAxis.setTickLabelFont(new Font("SansSerif", Font.PLAIN, 10)); // Cambia el tamaño y tipo de fuente
+
+        NumberAxis rangeAxis = (NumberAxis) plot.getRangeAxis();
+        rangeAxis.setTickLabelFont(new Font("SansSerif", Font.PLAIN, 10)); // Cambia el tamaño y tipo de fuente
+
+        gbc = sObjGraficos.getGridBagConstraints(0, 0, 1, 1, 1, 1, GridBagConstraints.BOTH, GridBagConstraints.CENTER, new Insets(0, 0, 0, 0), 10, 10);
         this.add(panel, gbc);
-        
+
 //        panelPoblacionComponent.getInterfazCatalogoComponent().getInterfazCatalogoTemplate().pack();
 //        panelPoblacionComponent.getInterfazCatalogoComponent().getInterfazCatalogoTemplate().repaint();
 //        panelPoblacionComponent.getInterfazCatalogoComponent().getInterfazCatalogoTemplate().setExtendedState(MAXIMIZED_BOTH);
     }
-    
+
 }
